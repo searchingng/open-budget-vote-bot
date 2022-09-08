2daeaed7ef918a23e5b27859a1dce8d96d54d8c9
Sirojiddin Kilichbaev <ssirojiddin2004@gmail.com>
2022-09-08 17:32:41 +0500

    returns errors

--- src/main/java/uz/everbestlab/votebudget/service/impl/SendMessageServiceImpl.java
+++ src/main/java/uz/everbestlab/votebudget/service/impl/SendMessageServiceImpl.java
@@ -72,7 +72,7 @@ public class SendMessageServiceImpl implements SendMessageService {
     }
 
     private String sendPhoneNumber(Long chatId, String text) {
-        String response = "📞 Iltimos telefon raqamini [__991234567__] ko'rinishida kiriting";
+        String response = "📞 Iltimos telefon raqamini [_991234567_] ko'rinishida kiriting";
         if (text.length() != 9)
             return response;
 
